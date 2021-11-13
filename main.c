ba2886a76e575b45894afc23e448374e5efa6e68
OND <csiyuki245@gmail.com>
2021-11-13 17:01:10 +0900

    UI作成中

--- main.c
+++ main.c
@@ -1,52 +1,47 @@
 #include <stdio.h>
 #include <string.h>
-
 #define MAXBUFSIZE 256
 
-void test(int a, int b)
-{
-    printf("ans = %d, ans2 = %d\n", a, b);
-
-}
-
-int add(int a, int b)
-{
-
-    return a + b;
-}
+#define TITLE   0 //起動時の最初のタイトル画面
+#define GAME    1 //ゲーム中
+#define RESULT  2 //リザルト画面
+#define END     3 //終了
 
 int main()
 {
-    int answer;
-    int ans2;
-    int condition = 0;
+    int status = TITLE;
+
     char string[MAXBUFSIZE];
    // printf("○×ゲーム\n");
    // printf("スタートしますか？ yes or no\n");
 
-    
-    answer = add(1, 3);
-    ans2 = add(5, 4);
-
-    test(answer, ans2);
-
-    fgets(string, MAXBUFSIZE, stdin);
-
-    
-
-    if (strcmp(string, "yes\n") == 0)
+    while ( status != END) 
     {
-        printf("Yesと入力しました。");
-    }
-    else
-    {
-        printf("input = %s test\n", string);
-    }
+        printf("○×ゲーム");
+        printf("ゲームをスタートしますか？");
 
-    while (condition < 10) 
-    {
-        printf("繰り返してます… %d\n", condition);
-        condition = condition + 1;
+        fgets(string, MAXBUFSIZE, stdin);
+        
+        if (strcmp(string, "yes\n") == 0)
+        {
+            printf("ゲーム中表示");
+        }
+        else
+        {
+            printf("input = %s 終了\n", string);
+        }
+
+        if (strcmp(string, "yes\n") == 0)
+        {
+            printf("ゲーム中表示");
+        }
+        else
+        {
+            printf("input = %s 終了\n", string);
+        }
+
+        printf("ゲームを終了？ or もう一回やりますか？ もう一回やるときはyes 終了するときはno を入力\n");
+
     }
 
     return 0;
